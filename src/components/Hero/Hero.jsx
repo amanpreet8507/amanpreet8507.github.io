be81ac9466d58b723a3ccba07c29a861c976a589
Amanpreet Kaur <amanpreet8507@gmail.com>
2024-06-06 14:53:47 -0700

    hero and about section mob responsiveness

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,20 +6,22 @@ import { Link } from "react-router-dom";
 const Hero = () => {
   return (
     <div className="hero">
-      <img className="hero__image" src={hero} alt="hero-image" />
-      <h1 className="hero__h1">
+      <img src={hero} alt="hero-image" />
+      <h1>
         <span className="hero__span">I'm Amanpreet Kaur,</span> software
         developer based in Canada.
       </h1>
-      <p className="hero__p">
+      <p>
         I am a frontend developer from Vancouver, Canada with 2+ years of
         experience
       </p>
       <div className="hero__action">
-        <Link to="/contact">
+        <Link to="/contact" className="hero__link">
           <div className="hero__connect">Connect With Me</div>
         </Link>
+        <Link to="" className="hero__link">
         <div className="hero__resume">My Resume</div>
+        </Link>
       </div>
     </div>
   );
