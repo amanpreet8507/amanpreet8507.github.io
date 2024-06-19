b24b8cd804dcfe8db5ddb94c14beb77ca31423ed
Amanpreet Kaur <amanpreet8507@gmail.com>
2024-06-19 12:57:18 -0700

    projects added

--- src/assets/data/mywork.js
+++ src/assets/data/mywork.js
@@ -1,31 +1,38 @@
 import inStock_img from "../images/InStockProject.png";
 import brainflix_img from "../images/brainflix.png";
+import foodify_img from "../images/foodify.png";
+
 const MyWorkData = [
   {
     id: 1,
     project_name: "In Stock",
-    description:"InStock in a fully responsive and full stack application for warehouses to keep a record of stock",
+    description:
+      "InStock in a fully responsive and full stack application for warehouses to keep a record of stock",
     project_img: inStock_img,
-    link: "https://github.com/amanpreet8507/instock-frontend"
+    link: "https://github.com/amanpreet8507/instock-frontend",
   },
   {
     id: 2,
     project_name: "Brainflix",
-    description:"InStock in a fully responsive and full stack application for warehouses to keep a record of stock",
+    description:
+      "Brainflix is a fully responsive website showing data for videos.",
     project_img: brainflix_img,
-    link: "https://github.com/amanpreet8507/amanpreet-kaur-brainflix"
-  },{
+    link: "https://github.com/amanpreet8507/amanpreet-kaur-brainflix",
+  },
+  {
     id: 3,
-    project_name: "In Stock",
-    description:"InStock in a fully responsive and full stack application for warehouses to keep a record of stock",
-    project_img: inStock_img,
-    link:""
-  },{
+    project_name: "Foodify",
+    description: "Foodify is full fledged app which helps cooking passionates to order ingredients to their door as per the recipe they want to make.",
+    project_img: foodify_img,
+    link: "https://github.com/amanpreet8507/foodify",
+  },
+  {
     id: 4,
-    project_name: "In Stock",
-    description:"InStock in a fully responsive and full stack application for warehouses to keep a record of stock",
+    project_name: "Expense Ease",
+    description:
+      "",
     project_img: inStock_img,
-    link:""
+    link: "",
   },
 ];
 export default MyWorkData;
