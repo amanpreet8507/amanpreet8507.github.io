1a933ac45de3e3f8e0963cad8620a92cffab4d36
Amanpreet Kaur <amanpreet8507@gmail.com>
2024-06-19 16:44:16 -0700

    my work section improved

--- src/assets/data/mywork.js
+++ src/assets/data/mywork.js
@@ -1,13 +1,14 @@
 import inStock_img from "../images/InStockProject.png";
 import brainflix_img from "../images/brainflix.png";
 import foodify_img from "../images/foodify.png";
+import bandsite_img from "../images/bandsite.png";
 
 const MyWorkData = [
   {
     id: 1,
     project_name: "In Stock",
     description:
-      "InStock in a fully responsive and full stack application for warehouses to keep a record of stock",
+      "InStock in a full stack application for warehouses to keep a record of inventories at different locations.",
     project_img: inStock_img,
     link: "https://github.com/amanpreet8507/instock-frontend",
   },
@@ -15,24 +16,25 @@ const MyWorkData = [
     id: 2,
     project_name: "Brainflix",
     description:
-      "Brainflix is a fully responsive website showing data for videos.",
+      "Brainflix is a fully responsive website showing data for trending videos.",
     project_img: brainflix_img,
     link: "https://github.com/amanpreet8507/amanpreet-kaur-brainflix",
   },
   {
     id: 3,
     project_name: "Foodify",
-    description: "Foodify is full fledged app which helps cooking passionates to order ingredients to their door as per the recipe they want to make.",
+    description:
+      "Foodify is full fledged app which helps cooking passionates to order ingredients to their door as per the recipe they want to make.",
     project_img: foodify_img,
     link: "https://github.com/amanpreet8507/foodify",
   },
   {
     id: 4,
-    project_name: "Expense Ease",
+    project_name: "Bandsite",
     description:
-      "",
-    project_img: inStock_img,
-    link: "",
+      "Bandsite is a fully responsive website that showcases the latest shows data from an api and let the users share their experience about shows by adding comments to the website.",
+    project_img: bandsite_img,
+    link: "https://github.com/amanpreet8507/amanpreet-kaur-bandsite",
   },
 ];
 export default MyWorkData;
